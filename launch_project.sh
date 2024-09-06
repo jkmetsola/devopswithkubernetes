4edf06ca7981f7c4ae5ee0e50fe780b23e78fdab
jkmetsola <jkmetsola@hotmail.fi>
2024-09-06 09:17:43 +0000

    Excercise 1.03 & 1.04: Create deployment.yaml

--- launch_project.sh
+++ launch_project.sh
@@ -2,16 +2,38 @@
 
 set -euo pipefail
 
-k3d cluster delete
-k3d cluster create --host-alias 0.0.0.0:host.docker.internal -a 2 --k3s-arg "--tls-san=host.docker.internal@server:0"
-KUBE_API_ADDRESS="$(kubectl config view -o jsonpath='{.clusters[?(@.name=="k3d-k3s-default")].cluster.server}')"
-PORT=$(echo "$KUBE_API_ADDRESS" | awk -F: '{print $NF}')
-kubectl config set clusters.k3d-k3s-default.server https://host.docker.internal:"$PORT"
-kubectl cluster-info
+RANDOM_STRING_APP=excercise101
+SIMPLE_WEB_SERVER_APP=excercise102
 
-docker build -f "excercise101/Dockerfile" -t excercise101:latest "excercise101"
-k3d image import excercise101:latest
-kubectl apply -f excercise101/deployment.yaml
+create_cluster() {
+    k3d cluster delete
+    k3d cluster create --host-alias 0.0.0.0:host.docker.internal -a 2 --k3s-arg "--tls-san=host.docker.internal@server:0"
+    KUBE_API_ADDRESS="$(kubectl config view -o jsonpath='{.clusters[?(@.name=="k3d-k3s-default")].cluster.server}')"
+    PORT=$(echo "$KUBE_API_ADDRESS" | awk -F: '{print $NF}')
+    kubectl config set clusters.k3d-k3s-default.server https://host.docker.internal:"$PORT"
+    kubectl cluster-info
+}
 
-docker build -f "excercise102/Dockerfile" -t excercise102:latest "excercise102"
-k3d image import excercise102:latest
+get_pod_name() {
+    kubectl get pods -l app="$1" -o jsonpath="{.items[0].metadata.name}"
+}
+
+deploy_apps() {
+    for APP in "$@"; do
+        docker build -f ${APP}/Dockerfile -t ${APP}:latest ${APP}
+        k3d image import ${APP}:latest
+        kubectl apply -f ${APP}/manifests/deployment.yaml
+    done
+}
+
+wait_apps() {
+    for APP in "$@"; do
+        pod_name="$(get_pod_name ${APP})"
+        kubectl wait --for=condition=Ready --timeout=30s pod/${pod_name}
+        kubectl logs ${pod_name}
+    done
+}
+
+create_cluster
+deploy_apps $RANDOM_STRING_APP $SIMPLE_WEB_SERVER_APP
+wait_apps $RANDOM_STRING_APP $SIMPLE_WEB_SERVER_APP
