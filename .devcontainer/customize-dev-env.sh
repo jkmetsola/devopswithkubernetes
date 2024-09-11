baafd93b1a4514031be13b0b7ca3c80a77f7a74b
jkmetsola <jkmetsola@hotmail.fi>
2024-09-11 11:50:19 +0000

    Improve bash history saving between sessions

--- .devcontainer/customize-dev-env.sh
+++ .devcontainer/customize-dev-env.sh
@@ -6,7 +6,7 @@ WORKSPACE_FOLDER="$(git rev-parse --show-toplevel)"
 # shellcheck source=.env
 source "$WORKSPACE_FOLDER"/.env
 
-cat "${WORKSPACE_FOLDER}/.devcontainer/bashrc-custompart" >> ~/.bashrc
+cat "${WORKSPACE_FOLDER}/.devcontainer/bashrc-custompart.sh" >> ~/.bashrc
 echo "
 git config --global user.email \"$GIT_EMAIL\"
 git config --global user.name \"$GIT_USER\"
