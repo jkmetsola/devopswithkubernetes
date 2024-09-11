3d2e4d84bbdcf8c7a43381dc6883f3d141ddd0f6
jkmetsola <jkmetsola@hotmail.fi>
2024-09-11 11:50:19 +0000

    Improve bash history saving between sessions

--- .devcontainer/bashrc-custompart.sh
+++ .devcontainer/bashrc-custompart.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 parse_git_branch() {
   if [ -n "$(git rev-parse --git-dir 2> /dev/null)" ]; then
     echo "($(git rev-parse --abbrev-ref HEAD)) "
@@ -6,13 +7,15 @@ parse_git_branch() {
 
 export LS_OPTIONS="--color=auto"
 eval "$(dircolors)"
-alias ls="ls $LS_OPTIONS"
-alias ll="ls $LS_OPTIONS -l"
-alias l="ls $LS_OPTIONS -lA"
+alias ls="ls \$LS_OPTIONS"
+alias ll="ls \$LS_OPTIONS -l"
+alias l="ls \$LS_OPTIONS -lA"
 
 
 PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
 PS1="${PS1}\[\033[01;33m\]\$(parse_git_branch)\[\033[00m\]"
 export PS1
 
+# shellcheck source=/dev/null
 source <(kubectl completion bash)
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
