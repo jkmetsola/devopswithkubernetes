09802da29cd29a1342c4afe4680cd91489899261
jkmetsola <jkmetsola@hotmail.fi>
2024-09-17 06:06:46 +0000

    Install yq

--- imagefiles/no-apt-packages-devenv.sh
+++ imagefiles/no-apt-packages-devenv.sh
@@ -5,4 +5,10 @@ install_k3d() {
     wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.7.3 bash
 }
 
+install_yq() {
+    wget https://github.com/mikefarah/yq/releases/download/v4.44.3/yq_linux_amd64 -O /usr/bin/yq
+    chmod +x /usr/bin/yq
+}
+
 install_k3d
+install_yq
