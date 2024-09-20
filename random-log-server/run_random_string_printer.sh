2991859e238fa08a2b16fa661070d2521109420b
jkmetsola <jkmetsola@hotmail.fi>
2024-09-20 08:58:32 +0000

    Exercise 1.11: Persisting data
    Let's share data between "Ping-pong" and "Log output" applications using persistent volumes.

--- random-log-server/run_random_string_printer.sh
+++ random-log-server/run_random_string_printer.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-random_string_printer/random_string_printer.py --logfile "${LOG_FILE}"
+random_string_printer/random_string_printer.py --logfile "${LOG_FILE}" --pongfile "${PONG_FILE}"
