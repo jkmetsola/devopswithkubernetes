b46d177a4581d76a80f2e6e44e89c497fab11317
jkmetsola <jkmetsola@hotmail.fi>
2024-11-13 21:57:27 +0000

    Improve git information at terminal

--- .devcontainer/bashrc-custompart.sh
+++ .devcontainer/bashrc-custompart.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-parse_git_branch() {
-  if [ -n "$(git rev-parse --git-dir 2> /dev/null)" ]; then
-    echo "($(git rev-parse --abbrev-ref HEAD)) "
-  fi
-}
 
 export LS_OPTIONS="--color=auto"
 eval "$(dircolors)"
@@ -11,9 +6,19 @@ alias ls="ls \$LS_OPTIONS"
 alias ll="ls \$LS_OPTIONS -l"
 alias l="ls \$LS_OPTIONS -lA"
 
+# shellcheck disable=SC1090
+. ~/git-prompt.sh
 
-PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
-PS1="${PS1}\[\033[01;33m\]\$(parse_git_branch)\[\033[00m\]"
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+
+git_info() {
+    __git_ps1 " (%s)"
+}
+
+PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]"
+PS1="${PS1}\[\033[01;33m\]\$(git_info) \[\033[00m\]\$ "
 export PS1
 
 # shellcheck source=/dev/null
