4fe4c41f32c22b621298739948e4945f540ddc96
jkmetsola <jkmetsola@hotmail.fi>
2024-11-13 19:50:57 +0000

    Excercise 3.02 Fixes
    It was identified that the loadbalancer did not work as intended
    with the custom class.
    
    - Removed the ingress class definition
    - Modified scripts so that those can handle the prefix
    - Started to use database in logprinter application

--- tools/launch_cluster.sh
+++ tools/launch_cluster.sh
@@ -4,15 +4,6 @@ set -euo pipefail
 
 WORKSPACE_FOLDER="$(git rev-parse --show-toplevel)"
 
-add_nginx_controller() {
-    helm install --debug ingress-nginx ingress-nginx \
-    --repo https://kubernetes.github.io/ingress-nginx \
-    --namespace ingress-nginx \
-    --create-namespace \
-    --version 4.11.3 \
-    --wait
-}
-
 add_loki(){
     helm repo add grafana https://grafana.github.io/helm-charts
     helm repo update
@@ -31,12 +22,10 @@ create_cluster() {
         --host-alias 0.0.0.0:host.docker.internal \
         --agents 2 \
         --k3s-arg "--tls-san=host.docker.internal@server:0" \
-        --k3s-arg="--disable=traefik@server:0" \
         --port 8081:80@loadbalancer \
         --api-port host.docker.internal:6550 \
         --wait
-    add_loki &
-    add_nginx_controller
+    add_loki
     exit 0
 }
 
