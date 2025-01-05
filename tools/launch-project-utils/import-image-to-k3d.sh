a451124e056a0e2f25650e76b947f245f5096159
jkmetsola <jkmetsola@hotmail.fi>
2025-01-05 14:03:27 +0000

    Enhance subprocess error handling in post-commit
    - Fix a bug in k3d parallel execution

--- tools/launch-project-utils/import-image-to-k3d.sh
+++ tools/launch-project-utils/import-image-to-k3d.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 
 IMAGE_TAG="$1"
+LOCK_FILE="$HOME/.k3d-lock"
+
+cleanup() {
+    rm "$LOCK_FILE"
+}
 
 get_image_sha() {
     if output="$(docker inspect --format='{{.Id}}' "$IMAGE_TAG" 2>&1)"; then
@@ -27,6 +32,12 @@ main() {
     IMAGE_SHA="$(get_image_sha)"
     if [ -z "${CI:-}" ]; then
         if ! image_available; then
+            while [[ -f "$LOCK_FILE" ]]; do
+                echo "Waiting for another k3d process to be finished..."
+                sleep 1
+            done
+            touch "$LOCK_FILE"
+            trap cleanup EXIT
             k3d image import "$IMAGE_TAG"
         fi
     fi
