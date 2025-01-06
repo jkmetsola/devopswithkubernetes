bba2a315c9dbc864d253a05dd3bd39d0a4e0712e
jkmetsola <jkmetsola@hotmail.fi>
2025-01-06 20:41:31 +0000

    Exercise 4.01: Readiness Probe
    By creating initcontainer that checks the
    database connection, probes can be properly
    used without unnecessary container resets.
    
    Without initcontainer, application would crash
    in the case of this project (per design).

---
+++ tools/testing-scripts/exercise-401-readiness-probes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+WORKSPACE_FOLDER="$(git rev-parse --show-toplevel)"
+# shellcheck source=.env
+source "$WORKSPACE_FOLDER/.env"
+# shellcheck source=.devcontainer/setupEnv.sh
+source "${SETUP_ENV_PATH}" "false"
+
+NAMESPACE=project-other-dev-jmetsola
+
+get_pod_status_and_sleep(){
+    kubectl get po --namespace "$NAMESPACE"
+    sleep 5
+}
+
+main(){
+    kubectl delete all --all --namespace "$NAMESPACE"
+    $BUILD_AND_APPLY_TOOL "$WORKSPACE_FOLDER/project-other/apps/pingpong" "$NAMESPACE"
+    $BUILD_AND_APPLY_TOOL "$WORKSPACE_FOLDER/project-other/apps/logserver" "$NAMESPACE"
+    get_pod_status_and_sleep
+    $BUILD_AND_APPLY_TOOL "$WORKSPACE_FOLDER/project-common/databases/postgres" "$NAMESPACE"
+    for _ in {1..10}; do
+        if get_pod_status_and_sleep | grep "2/2" > /dev/null; then
+            get_pod_status_and_sleep
+            exit 0
+        fi
+    done
+}
+
+main
